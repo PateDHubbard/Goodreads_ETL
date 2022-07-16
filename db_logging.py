e35d3054e8099a58bc711e3cca012bd62ee398a1
Pate Hubbard <paytondhubbard@gmail.com>
2022-07-16 17:11:51 -0600

    add db_logging.py file

---
+++ db_logging.py
@@ -0,0 +1,18 @@
+import requests
+import uuid
+
+
+# Log group is set just once for the entire session
+LOG_GROUP = str(uuid.uuid1())
+URL = "https://king-prawn-app-ebwri.ondigitalocean.app/logging/goodreads_etl"
+
+
+def send_log_to_db(log_name, log_status, log_notes):
+    data = {
+        "log_group": LOG_GROUP,
+        "log_name": log_name,
+        "log_status": log_status,
+        "log_notes": log_notes
+    }
+    response = requests.post(URL, data=data)
+    return response
